8c4f1b3c6458414c49196c81d28d703974b74628
cqm <mtdxc@126.com>
2023-08-16 11:07:53 +0800

    处理定标精度问题

--- Experiments/2video.py
+++ Experiments/2video.py
@@ -9,8 +9,23 @@ if len(sys.argv) > 1:
     choosen_video = sys.argv[1]
 
 cap = cv2.VideoCapture(choosen_video)
-width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
-height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+ret, img = cap.read()
+if not ret:
+    print('unable to open file ', choosen_video)
+    exit()
+# 校准参数    
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+# 只算k1畸变，算多的话，好像误差更大
+flag = cv2.CALIB_FIX_K3 | cv2.CALIB_FIX_K2 | cv2.CALIB_FIX_TANGENT_DIST
+
+font = cv2.FONT_HERSHEY_SIMPLEX
+color = (255, 0, 0)
+
+np.set_printoptions(precision=2)
+np.set_printoptions(suppress=True)
+
+width = img.shape[1] #cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+height = img.shape[0] #cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
 fps = cap.get(cv2.CAP_PROP_FPS)
 print('%s image size(%dx%d@%d)' % (choosen_video, width, height, fps))
 
@@ -20,18 +35,19 @@ realh = 273
 scale = min(width / realw, height / realh)
 realw = int(realw * scale)
 realh = int(realh * scale)
-print('real size %dx%d' % (realw, realh))
 
-nPoint = 0
-# 3d坐标
+# 3d坐标点数组
 point3d = []
-# 初始化2d坐标为0，长度与point3d相同
+# 2d坐标点，个数与point3d一样，初始为0
 point2d = []
-# 当前选择点    
+# 当为0时则手工指定映射点，为n^2时则按均分方式自动计算
+nPoint = 0
+# 当前选择点
 pos = 0
-# 第二直角边位置
+
+# 截取矩形的第二个(右上)点位置
 pos2 = 1
-# 第三直角边位置
+# 截取矩形的第三个(左下)点位置
 pos3 = 2
 if nPoint > 0: # 平均生成3d点
     count = int(np.sqrt(nPoint))
@@ -50,48 +66,42 @@ else: # 手工选择映射点
     scale = 1
     # calibrateCamera 必须有8个点，15个行列式
     point3d = [[0,0,0], [realw,0,0], [0,300,0], [realw/2,300,0], [realw,300,0], [0,realh,0], [realw/2, realh, 0], [realw,realh,0]]
-    # 编码7个对应点
-    point2d = [[447, 160], [945, 156], [406, 200], [700, 188], [984, 195], [256, 361], [702, 356], [1130, 352]]
-    pos = 7
+    # 硬编码7个对应点
+    point2d = [[447, 161], [945, 156], [408, 200], [699, 188], [985, 195], [258, 353], [703, 349], [1123, 345]]
     pos2 = 1
     pos3 = 5
+    # start with last no
+    pos = 7
 
+print('real size %dx%d' % (realw, realh))
 nPoint = len(point3d)
-print('use %d point3ds' % nPoint, point3d)
+print('use %d point3ds:' % nPoint, point3d)
 
 while len(point2d) < nPoint:
     point2d.append([0,0])
 
-ret, img = cap.read()
-if not ret:
-    print('unable to open file ', choosen_video)
-    exit()
-
-
-
+# point2d 标定点选择函数
 def draw_circle(event,x,y,flags,param):
-    global point2d
-    global pos, nPoint
+    global point2d, pos
     # 双击后设置当前点，并移到下一个
     if event==cv2.EVENT_LBUTTONDBLCLK:
         point2d[pos] = [x,y]
         print(pos, '=', point2d[pos])
         pos = pos + 1
 
+# 手工选择标定点
 selname = 'select %d points of %dx%d' % (nPoint, realw, realh)
 cv2.namedWindow(selname,cv2.WINDOW_NORMAL)
 cv2.setMouseCallback(selname, draw_circle)
 
-font = cv2.FONT_HERSHEY_SIMPLEX
-color = (255, 0, 0)
-# 手工选择标定点
 while(pos < nPoint):
     frame = img.copy()
     for i in range(nPoint):
-        # 标定点
+        # 标出标定点索引和对应的3d坐标
         cv2.circle(frame, point2d[i], 4, (0, 255, 0), -1)
         cv2.putText(frame, "%d(%d,%d)" % (i, point3d[i][0], point3d[i][1]), 
                 (point2d[i][0] + 5, point2d[i][1]), font, 0.7, color, 2)
+    # 提示选择下一个标定点
     cv2.putText(frame, "select %d point(%d,%d)" % (pos, point3d[pos][0], point3d[pos][1]), 
             (int(width/2) - 50, 20), font, 0.7, color, 2)
     cv2.imshow(selname, frame)
@@ -99,7 +109,7 @@ while(pos < nPoint):
     key = cv2.waitKey(20) & 0xFF
     if key==27:  #按ESC退出
         exit(0)
-    elif key == ord('s'):
+    elif key == ord('s'): # save 推出
         break
     elif key >= ord('0') or key <= ord('9'):
         # 重设某个点
@@ -108,18 +118,21 @@ while(pos < nPoint):
             pos = newPos
             print('reset pos', pos)
     elif key == ord('n'):
-        # 转到下一帧进行标定
+        # 转到下一帧重新进行标定
+        ret, img = cap.read()
+        pos = 0
         point2d = []
         for i in range(nPoint):
             point2d.append((0,0))
-        pos = 0
-        ret, img = cap.read()
 
+# 用一组数据校准摄像头
 calibrate = False
 objpoints = [np.array(point3d, dtype=np.float32)]
 imgpoints = [np.array(point2d, dtype=np.float32)]
-flag = cv2.CALIB_FIX_K3 #| cv2.CALIB_FIX_K2 | cv2.CALIB_FIX_TANGENT_DIST
-ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, img.shape[:2], None,None, None, None, flag)
+mapx, mapy, nmtx = (None, None, None)
+# imgSize必须设对，否则cv2.remap会出现截断错误..
+imgSize = (width, height)
+ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, imgSize, None, None, None, None, flag, criteria)
 if ret:
     calibrate = ret
     print('martix', mtx)
@@ -127,32 +140,28 @@ if ret:
     print('rvecs', rvecs)
     print('tvecs', tvecs)
 
-def undistort(frame):
-    h,w=frame.shape[:2]
-    k=np.array(mtx)
-    d=np.array(dist[0])
-    mapx,mapy=cv2.initUndistortRectifyMap(k,d,None,k,(w,h),5)
-    return cv2.remap(frame,mapx,mapy,cv2.INTER_LINEAR)
-
-def undistort2(frame):
-    h,w=frame.shape[:2]
     # 如果缩放系数 alpha = 0,返回的非畸变图像会带有最少量的不想要的像素。它甚至有可能在图像角点去除一些像素。
     # 如果 alpha = 1,所有像素都会被返回,还有一些黑图像。它还会返回一个 ROI 图像,我们可用来对结果进行裁剪。
-    newcameramtx, roi=cv2.getOptimalNewCameraMatrix(mtx,dist,(w,h),1,(w,h))
-    # undistort
-    dst = cv2.undistort(frame, mtx, dist, None, newcameramtx)
-    return dst
+    print('getOptimalNewCameraMatrix1', cv2.getOptimalNewCameraMatrix(mtx,dist,imgSize,1)) # 1 返回带黑边
+    print('getOptimalNewCameraMatrix0', cv2.getOptimalNewCameraMatrix(mtx,dist,imgSize,0)) # 0 没黑边, 切掉部分像素
+    # 必须设为0，否则会出黑边，且对不上
+    nmtx,roi=cv2.getOptimalNewCameraMatrix(mtx,dist,imgSize,0)
+    # cv2.undistort(img, mtx, dist, None, nmtx)
+
+    mapx,mapy = cv2.initUndistortRectifyMap(mtx,dist,None,nmtx,imgSize,cv2.CV_32FC1)
+    # cv2.remap(img, mapx, mapy, cv2.INTER_LINEAR)
 
 # 填充2d矩形坐标点
 ptRect1 = np.array([point2d[0][:2], point2d[pos2][:2], point2d[pos3][:2], point2d[-1][:2]], dtype = "float32")
 print('rect1', ptRect1)
-# 去畸变, 暂时没用
-print(cv2.undistortPoints(ptRect1, mtx, dist, ptRect1))
+# 去畸变, 要指定P=mtx，否则按归一化输出，值特别小
+ptRect1 = cv2.undistortPoints(ptRect1, mtx, dist, P=mtx).reshape(-1,2)
+print('undistort rect1', ptRect1)
 # 填充3d矩形
 ptRect2 = np.array([point3d[0][:2], point3d[pos2][:2], point3d[pos3][:2], point3d[-1][:2]], dtype = "float32")
 print('rect2', ptRect2)
 M = cv2.getPerspectiveTransform(ptRect1,ptRect2)
-print('M', M)
+print('Perspective martix', M)
 
 def getMapPos(point):
     # 坐标映射矩阵
@@ -170,10 +179,12 @@ while 1:
     if not ret:
         break
     if calibrate:
-        img = undistort2(img)
+        # initUndistortRectifyMap+remap效率会高点
+        img = cv2.remap(img, mapx, mapy, cv2.INTER_LINEAR)
+        # img = cv2.undistort(img, mtx, dist, None, nmtx)
     cv2.imshow('img', img)
     out_img = cv2.warpPerspective(img,M,(realw,realh))
-    cv2.imshow('trans', out_img)
+    cv2.imshow('clip', out_img)
     k = cv2.waitKey(30)
     if k == ord('q') or k == 27:
         break
--- calibrate.py
+++ calibrate.py
@@ -3,7 +3,10 @@ import cv2
 
 # termination criteria
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
-flag = cv2.CALIB_FIX_K3
+flag = cv2.CALIB_FIX_K3# | cv2.CALIB_FIX_K2 | cv2.CALIB_FIX_TANGENT_DIST
+
+np.set_printoptions(precision=2)
+np.set_printoptions(suppress=True)
 # 结论：
 # 棋盘格必须最少3x3, 但此时返回的dist误差较大
 # 棋盘数只需要1个就能calibrateCamera
@@ -17,7 +20,7 @@ imgname = 'calibrate with %dx%d' % grid
 objp = np.zeros((grid[0] * grid[1], 3), np.float32)
 objp[:,:2] = np.mgrid[0:grid[0],0:grid[1]].T.reshape(-1,2)
 objp *= 28.7 #MM
-print(objp)
+print('objp=', objp)
 
 # Arrays to store object points and image points from all the images.
 objpoints = [] # 3d point in real world space
@@ -25,19 +28,18 @@ imgpoints = [] # 2d points in image plane.
 
 calibrate = False
 global ret, mtx, dist, rvecs, tvecs
-
+global nmtx, mapx, mapy
 def undistort(frame):
-    h,w=frame.shape[:2]
-    k=np.array(mtx)
-    d=np.array(dist[0])
-    mapx,mapy=cv2.initUndistortRectifyMap(k,d,None,k,(w,h),5)
+    h,w = frame.shape[:2]
+    nmtx = mtx # cv2.getOptimalNewCameraMatrix(mtx,dist,(w,h),0)
+    mapx,mapy = cv2.initUndistortRectifyMap(mtx,dist,None,nmtx,(w,h),cv2.CV_32FC1)
     return cv2.remap(frame,mapx,mapy,cv2.INTER_LINEAR)
 
 def undistort2(frame):
-    h,w=frame.shape[:2]
+    h,w = frame.shape[:2]
     # 如果缩放系数 alpha = 0,返回的非畸变图像会带有最少量的不想要的像素。它甚至有可能在图像角点去除一些像素。
     # 如果 alpha = 1,所有像素都会被返回,还有一些黑图像。它还会返回一个 ROI 图像,我们可用来对结果进行裁剪。
-    newcameramtx, roi=cv2.getOptimalNewCameraMatrix(mtx,dist,(w,h),1,(w,h))
+    newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx,dist,(w,h),1)
     # undistort
     dst = cv2.undistort(frame, mtx, dist, None, newcameramtx)
     return dst
@@ -47,8 +49,9 @@ def undistort2(frame):
     return dst
 
 def undistort1(frame):
-    h,w=frame.shape[:2]
-    newcameramtx, roi=cv2.getOptimalNewCameraMatrix(mtx,dist,(w,h),0,(w,h))
+    h,w = frame.shape[:2]
+    # 不出黑边
+    newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx,dist,(w,h),0)
     # undistort
     return cv2.undistort(frame, mtx, dist, None, newcameramtx)
 
@@ -58,7 +61,8 @@ frameNo = 0
 lastChessNo = 0
 
 # 打开摄像头
-cap = cv2.VideoCapture(0)#"rtsp://192.168.24.246/stream0")
+cap = cv2.VideoCapture(0) # "rtsp://192.168.24.246/stream0")
+cap.set(cv2.CAP_PROP_BUFFERSIZE, 0)
 while(cap.isOpened()): #循环读取每一帧0
     ret, img = cap.read()
     if not(ret):
@@ -68,8 +72,10 @@ while(cap.isOpened()): #循环读取每一帧0
     h,  w = img.shape[:2]
     if calibrate:
         # undistort
-        dst = undistort(img)
-
+        # dst = cv2.undistort(img, mtx, dist, None, None)
+        dst = cv2.undistort(img, mtx, dist, None, nmtx)
+        # dst = cv2.remap(img, mapx, mapy, cv2.INTER_LINEAR)
+        # dst = undistort1(img)
         # show image
         cv2.putText(dst, 'frame %d, undistort'%frameNo, (0,20), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0), 2)
         cv2.imshow(imgname, dst)
@@ -93,16 +99,22 @@ while(cap.isOpened()): #循环读取每一帧0
                 lastChessNo = frameNo
 
             # 检验N个棋盘后进行相机校准，可修改这个值进行
-            if len(objpoints) > 0:
-                #print('objpoints', objpoints)
-                #print('imgpoints', imgpoints)
-                ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None,None, None, None, flag)
+            if len(objpoints) > 2:
+                if grid[0] * grid[1] < 25:
+                    print('imgpoints', imgpoints)
+                imgSize = (w,h)
+                ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, imgSize, None,None, None, None, flag)
                 if ret:
                     calibrate = ret
                     print('mtx', mtx)
-                    print('dist畸变值', dist)
-                    print('rvecs 旋转（向量）外参', rvecs)
-                    print('tvecs 平移（向量）外参', tvecs)
+                    print('dist', dist)
+                    print('rvecs', rvecs)
+                    print('tvecs', tvecs)
+                    # 其中nmtx == mtx
+                    nmtx, roi = cv2.getOptimalNewCameraMatrix(mtx,dist,imgSize,0)
+                    # 这些参数都可提前计算，以提高效率
+                    mapx,mapy = cv2.initUndistortRectifyMap(mtx,dist,None,nmtx,imgSize,cv2.CV_32FC1)
+                    print('nmtx', nmtx)
                     print('==============================')
                 else:
                     print('calibrateCamera error')
